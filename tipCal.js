11e440ee6ad3d7a4d9c8b5cd9ee0d18678f4d26b
Miriam77 <miriam.macier@interapthq.com>
2016-09-29 13:21:03 -0400

    got tip calculator working

--- tipCal.js
+++ tipCal.js
@@ -1,26 +1,48 @@
+
 $(function () {
 
 
-    function getTip() {
-        var amount = $("#amount").val();
-        var percent = $("#service").val();
-        var tip = amount * percent;
+    $(function () {
+        $("button#calculate").click(function (ev) {
+            var amount = $("#amount").val();
+            var percent = $("#service").val();
+            var people = $("#people").val();
+
+            if(amount === "") {
+                alert("Please enter amount");
+                return;
+            }
+            // sets people value to 1, if nothing entered
+            if(people === "" || people <=1) {
+                people = 1;
+            }
+              var tip = amount * percent / people;
+              tip = Math.round(tip * 100)/100;
+              tip = tip.toFixed(2);
+
+            console.log("amount" + amount);
+            console.log("percent" + percent);
+            console.log(tip);
+
+            $("#tip").append(tip);
 
 
-        var total = tip;
-        console.log("amount" + amount);
-        console.log("percent" + percent);
-        console.log(tip);
 
-        $("#tip").val(tip.toFixed(2));
-        $("#total").val(total.toFixed(2));
-    }
 
-    $("#calculate").click(getTip);
-    $("#tip").append(tip);
 
-    console.log("dumbass")
 
+
+            // $(document).ready(function () {
+            //     //called when key is pressed in textbox
+            //     $(".onlynum").keypress(function (e) {
+            //         //if the letter is not digit then display error and don't type anything
+            //         if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+            //             //display error message
+            //             $("#errmsg").html("Digits Only").show().fadeOut("slow");
+            //             return false;
+
+        });             
+    });
 });
 
 
